cf91747dcbc630dcab840249cc3d3744ca01cc93
Anvar MS <anvarms2005@gmail.com>
2026-02-03 18:50:22 +0530

    Ray pos mismatch fixed

--- Assets/Scripts/SpiderBoddyController.cs
+++ Assets/Scripts/SpiderBoddyController.cs
@@ -1,9 +1,12 @@
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 public class SpiderBoddyController : MonoBehaviour
 {
-    [SerializeField] float speed = 5f;
-    [SerializeField] float boddyHeightOffset = 1.0f;
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float heightLearpSpeed = 5f;
+    [SerializeField] float rotationSlearpSpeed = 5f;
+    [SerializeField] Vector3 boddyHeightOffset;
     [SerializeField] List<SpiderLegController> legControllers;
     Rigidbody rb;
     Vector3 moveInput = Vector3.zero;
@@ -22,15 +25,42 @@ public class SpiderBoddyController : MonoBehaviour
 
         // Smoother movement visual when camera/objects move; optional but helpful
         rb.interpolation = RigidbodyInterpolation.Interpolate;
+
+      
+
     }
 
     void Update()
     {
        MoveSpider();
         // Update boddy height based on leg positions
-
+        AlligneBody();
     }
 
+    private void AlligneBody()
+    {
+        if (legControllers.Count == 0) return;
+        int groundedLegs = 0;
+        Vector3 averageLegPosition = Vector3.zero;
+        Vector3 averageNormal = Vector3.zero;
+        foreach (var leg in legControllers)
+        {
+            if (!leg.IsGrounded) continue;
+
+            averageLegPosition += leg.GetPosition();
+            averageNormal += leg.GetHitPoint().normal;
+            groundedLegs++;
+        }
+        if (groundedLegs < 2)
+            return;
+        averageLegPosition /= groundedLegs;
+        averageNormal /= groundedLegs;
+        Quaternion rotationFromUpToNormal = Quaternion.FromToRotation(transform.up, averageNormal);
+        Vector3 targetBoddyPosition = new Vector3(transform.position.x, averageLegPosition.y, transform.position.z) + boddyHeightOffset;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotationFromUpToNormal * transform.rotation, Time.deltaTime * rotationSlearpSpeed);
+        transform.position = Vector3.Lerp(transform.position, targetBoddyPosition, Time.deltaTime * heightLearpSpeed);
+    }
 
     private void MoveSpider()
     {
@@ -50,7 +80,7 @@ public class SpiderBoddyController : MonoBehaviour
         }
 
         // Move using Rigidbody.MovePosition to respect physics & collisions
-        Vector3 newPosition = rb.position + moveInput * speed * Time.fixedDeltaTime;
+        Vector3 newPosition = rb.position + moveInput * moveSpeed * Time.fixedDeltaTime;
         rb.MovePosition(newPosition);
     }
     
--- Assets/Scripts/SpiderLegController.cs
+++ Assets/Scripts/SpiderLegController.cs
@@ -1,49 +1,50 @@
 
 using System.Collections;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpiderLegController : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] Transform boddyTrasform;
-     Transform footIkTarget;
+    Transform footIkTarget;
     [SerializeField] private SpiderLegController pairedLeg;
 
     [Header("Settings")]
-    [SerializeField] float stepThreshold=2;
-    [SerializeField] float stepDistance=1;
-    [SerializeField] float stepHeight=0.7f;
-    [SerializeField] float stepSpeed =10;
-    [SerializeField] float raycastHeightOffset=2;
-   
+    [SerializeField] float stepThreshold = 2;
+    [SerializeField] float stepDistance = 1;
+    [SerializeField] float stepHeight = 0.7f;
+    [SerializeField] float stepSpeed = 10;
+    [SerializeField] float raycastHeightOffset = 2;
+
     // Private variables
     private Vector3 defaultLocalOffset;
     private Vector3 FootPose;
     private Vector3 desairedFootPose;
     private Vector3 lastBoadyPos;
     private Vector3 bodyVelocity;
-
+    private float maxLegLength;
     // Raycasting variables
-    private Vector3 rayOrigin; 
+    private Vector3 rayOrigin;
     private RaycastHit hitPoint;
 
     //animation variables
-   [SerializeField] private bool isSteping = false;
+    [SerializeField] private bool isSteping = false;
 
     // Public properties
     public bool IsGrounded { get { return !isSteping; } }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      if(footIkTarget==null)
+        if (footIkTarget == null)
         {
             footIkTarget = transform;
         }
         defaultLocalOffset = boddyTrasform.InverseTransformPoint(footIkTarget.position);
         FootPose = footIkTarget.position;
         lastBoadyPos = boddyTrasform.position;
-
+        maxLegLength = Vector3.Distance(boddyTrasform.position, footIkTarget.position);
 
     }
 
@@ -62,18 +63,24 @@ public class SpiderLegController : MonoBehaviour
 
         if (isSteping)
             return;
-        
-       
-        desairedFootPose = boddyTrasform.TransformPoint(defaultLocalOffset + Vector3.forward) + velocityOffset;
-        
 
-        if( Vector3.Distance(FootPose, desairedFootPose) > stepThreshold)
+        Vector3 localPrediction = defaultLocalOffset + Vector3.forward * stepDistance;
+
+
+        desairedFootPose = boddyTrasform.TransformPoint(localPrediction) + velocityOffset;
+
+       Vector3 vectorFromBody = desairedFootPose - boddyTrasform.position;
+
+        if (vectorFromBody.magnitude > maxLegLength)
+            desairedFootPose = boddyTrasform.position + vectorFromBody.normalized * maxLegLength;
+
+        if (Vector3.Distance(FootPose, desairedFootPose) > stepThreshold)
         {
-          rayOrigin = desairedFootPose + Vector3.up * raycastHeightOffset;
-            if(Physics.Raycast(rayOrigin, Vector3.down, out hitPoint, raycastHeightOffset*2))
+            rayOrigin = desairedFootPose + Vector3.up * raycastHeightOffset;
+            if (Physics.Raycast(rayOrigin, Vector3.down, out hitPoint, raycastHeightOffset * 3))
             {
-                
-                if (!isSteping &&pairedLeg.IsGrounded)
+
+                if (!isSteping && pairedLeg.IsGrounded)
                     StartCoroutine(MoveFoot());
             }
         }
@@ -81,13 +88,13 @@ public class SpiderLegController : MonoBehaviour
 
 
 
-   private IEnumerator MoveFoot()
+    private IEnumerator MoveFoot()
     {
         isSteping = true;
         Vector3 startpos = footIkTarget.position;
-        Vector3 endPos =hitPoint.point;
+        Vector3 endPos = hitPoint.point;
         float t = 0;
-        while ( t < 1)
+        while (t < 1)
         {
             t = Mathf.Clamp01(t + stepSpeed * Time.deltaTime);
             Vector3 targetPos = Vector3.Lerp(startpos, endPos, t);
@@ -99,13 +106,20 @@ public class SpiderLegController : MonoBehaviour
         isSteping = false;
     }
 
-
+    public Vector3 GetPosition()
+    {
+        return FootPose;
+    }
+    public RaycastHit GetHitPoint()
+    {
+        return hitPoint;
+    }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawSphere(desairedFootPose, 0.3f);
         Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(rayOrigin, rayOrigin + Vector3.down * raycastHeightOffset*2);
+        Gizmos.DrawLine(rayOrigin, rayOrigin + Vector3.down * raycastHeightOffset * 2);
         Gizmos.color = Color.cyan;
         Gizmos.DrawSphere(FootPose, 0.3f);
 
